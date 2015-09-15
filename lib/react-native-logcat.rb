a0e941d6205518937b4686dbeeed4d6dd027a006
cesar ferreira <cesar.manuel.ferreira@gmail.com>
2015-09-15 15:34:10 +0100

    added timestamp to the messages

--- lib/react-native-logcat.rb
+++ lib/react-native-logcat.rb
@@ -3,17 +3,26 @@ require 'colorize'
 
 module ReactNativeLogCat
 
-  def self.get_type(str)
+  def self.add_timestamp(str)
+    timestamp = Time.now.strftime("%H:%M:%S")
+    return "[#{timestamp}] #{str}"
+  end
+
+  def self.transform_line(str)
 
     if str[0..1] == "D/"
-      return str.green
+      return add_timestamp(str.green)
     end
 
     if str[0..1] == "W/"
-      return str.yellow
+      return add_timestamp(str.yellow)
     end
 
-    return str
+    if str[0..1] == "E/"
+      return add_timestamp(str.red)
+    end
+
+    return add_timestamp(str)
   end
 
   def self.start
@@ -26,7 +35,7 @@ module ReactNativeLogCat
     }'
 
     IO.popen(cmd).each do |line|
-      puts self.get_type(line.chomp)
+      puts self.transform_line(line.chomp)
     end
   end
 
